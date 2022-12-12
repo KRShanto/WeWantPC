86abc8c5966c65137bbba656fb10bee0ba4a11cf
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-12 20:07:03 +0600

    Add models

---
+++ src/models.rs
@@ -0,0 +1,26 @@
+use crate::schema::users;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Serialize, Deserialize, Debug)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub email: String,
+    #[serde(skip_serializing)]
+    #[serde(skip_deserializing)]
+    pub password: String,
+    pub role: String,
+    pub created_at: Option<chrono::NaiveDateTime>,
+}
+
+#[derive(Insertable, Serialize, Deserialize, Debug)]
+#[diesel(table_name = users)]
+pub struct NewUser {
+    pub name: String,
+    pub email: String,
+    pub password: String,
+    #[serde(skip_serializing)]
+    #[serde(skip_deserializing)]
+    pub role: String,
+}
