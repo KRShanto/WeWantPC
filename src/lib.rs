a8bde295d86949a23affe0ca81ddf4c19c28db9f
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-12 20:08:19 +0600

    Add `login` route for login the user

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+pub mod models;
+pub mod routes;
+pub mod schema;
+
+pub const SESSION_NAME: &str = "user_id";
+
+pub use serde::{Deserialize, Serialize};
+
+pub type DbPool = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>;
+
+/// Roles for maintaining an ecommerce site
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Role {
+    Admin,
+    User,
+    Staff,
+}
+
+impl From<String> for Role {
+    fn from(role: String) -> Self {
+        match role.as_str() {
+            "admin" => Role::Admin,
+            "user" => Role::User,
+            "staff" => Role::Staff,
+            _ => Role::User,
+        }
+    }
+}
+
+impl From<Role> for String {
+    fn from(role: Role) -> Self {
+        match role {
+            Role::Admin => "admin".to_string(),
+            Role::User => "user".to_string(),
+            Role::Staff => "staff".to_string(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ResponseType {
+    AlreadyExists,
+    Success,
+    NotFound,
+    ServerError,
+}
---
+++ src/routes/login.rs
@@ -0,0 +1,68 @@
+use crate::models::User;
+use crate::ResponseType;
+use crate::SESSION_NAME;
+use actix_session::Session;
+use actix_web::{post, web, web::block, HttpResponse, Responder};
+use argon2::{
+    password_hash::{PasswordHash, PasswordVerifier},
+    Argon2,
+};
+use diesel::prelude::*;
+use log::warn;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LoginUser {
+    email: String,
+    password: String,
+}
+
+/// Login a user
+///
+/// It accepts a json object of the [`LoginUser`] struct.
+/// It will first check if the user exists.
+/// Then it will verify the password using argon2.
+/// After that it will log the user in using the session.
+#[post("/login")]
+pub async fn login_route(
+    session: Session,
+    pool: web::Data<crate::DbPool>,
+    user_to_login: web::Json<LoginUser>,
+) -> impl Responder {
+    let user_to_login = user_to_login.into_inner();
+    let user = block(move || {
+        use crate::schema::users::dsl::*;
+        let mut conn = pool.get().unwrap();
+        users
+            .filter(email.eq(&user_to_login.email))
+            .first::<User>(&mut conn)
+            .ok()
+    })
+    .await
+    .unwrap();
+
+    if user.is_none() {
+        warn!("No user present");
+        return HttpResponse::Ok().json(ResponseType::NotFound);
+    }
+
+    let user = user.unwrap();
+
+    let password = block(move || {
+        let hash = PasswordHash::new(&user.password).unwrap();
+        Argon2::default()
+            .verify_password(user_to_login.password.as_bytes(), &hash)
+            .is_ok()
+    })
+    .await
+    .unwrap();
+
+    if !password {
+        warn!("Password is incorrect");
+        return HttpResponse::Ok().json(ResponseType::NotFound);
+    }
+
+    session.insert(SESSION_NAME, user.id).unwrap();
+
+    HttpResponse::Ok().json(ResponseType::Success)
+}
