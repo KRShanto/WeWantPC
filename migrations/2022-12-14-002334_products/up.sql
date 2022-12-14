28cb626efc3336ebe34bc2d327053eebe057840b
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-14 09:19:32 +0600

    Include `products` table

---
+++ migrations/2022-12-14-002334_products/up.sql
@@ -0,0 +1,15 @@
+-- Your SQL goes here
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    name_variable VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    price INT NOT NULL,
+    quantity INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    category_id INTEGER NOT NULL REFERENCES categories(id) ON DELETE CASCADE,
+    brand VARCHAR(255) NOT NULL    
+)
