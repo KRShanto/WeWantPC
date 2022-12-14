e7c1c120ab1306277f45c4fbc6225d37cd27f19c
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-14 11:37:02 +0600

    Use sass rather than css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Navbar />
-            <Component {...pageProps} />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
