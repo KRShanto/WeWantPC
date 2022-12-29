d870aecc6e811d53465541bd8f06b75fb3a8722c
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-29 13:26:06 +0600

    Add a page loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,41 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import LoadingBar from "react-top-loading-bar";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const handleStart = (url: string) => {
+      setProgress(30);
+    };
+    const handleComplete = (url: string) => {
+      setProgress(100);
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
   return (
     <>
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        height={3}
+        onLoaderFinished={() => setProgress(0)}
+      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />
