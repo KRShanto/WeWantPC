8f70e02cdeca29a670f7eacfe4af03f3920f164f
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-29 13:23:54 +0600

    Create navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,22 @@ export default function Navbar() {
 
   return (
     <nav>
-      <ul>
+      <div className="logo">WeWantPC</div>
+
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <button>Search</button>
+      </div>
+
+      <div className="links">
         <Link href="/">Home</Link>
         <Link href="/pc-components">PC Components</Link>
         <Link href="/compare">Compare</Link>
-        <Link href="/login">Login/Register</Link>
+        <Link href="/login">Login</Link>
         <Link href="/cart">Cart</Link>
         <Link href="/help">Help</Link>
         {/* TODO <Link href="/dashboard">Dashboard</Link> */}
-      </ul>
+      </div>
     </nav>
   );
 }
