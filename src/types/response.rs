d1088f58a3b03ed978511a7fe59844f9d05fe633
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-04 13:06:09 +0600

    Create custom Respnose struct

--- src/types/response.rs
+++ src/types/response.rs
@@ -1,3 +1,5 @@
+use actix_web::HttpResponse;
+
 use crate::*;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -10,4 +12,92 @@ pub enum ResponseType {
     NoPermission,
     InvalidInput,
     Unauthorized,
+    IncorrectPassword,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Response {
+    response_type: ResponseType,
+    message: Option<String>,
+    data: Option<serde_json::Value>,
+}
+
+impl Response {
+    pub fn already_exists() -> Self {
+        Self {
+            response_type: ResponseType::AlreadyExists,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn success() -> Self {
+        Self {
+            response_type: ResponseType::Success,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn not_found() -> Self {
+        Self {
+            response_type: ResponseType::NotFound,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn server_error() -> Self {
+        Self {
+            response_type: ResponseType::ServerError,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn no_permission() -> Self {
+        Self {
+            response_type: ResponseType::NoPermission,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn invalid_input() -> Self {
+        Self {
+            response_type: ResponseType::InvalidInput,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn unauthorized() -> Self {
+        Self {
+            response_type: ResponseType::Unauthorized,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn incorrect_password() -> Self {
+        Self {
+            response_type: ResponseType::IncorrectPassword,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn msg(mut self, message: &str) -> Self {
+        self.message = Some(message.to_string());
+        self
+    }
+
+    pub fn data(mut self, data: serde_json::Value) -> Self {
+        self.data = Some(data);
+        self
+    }
+
+    pub fn send(&self) -> HttpResponse {
+        HttpResponse::Ok().json(self)
+    }
 }
