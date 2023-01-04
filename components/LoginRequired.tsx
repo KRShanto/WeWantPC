2e4d0391dee68a4ad25f21488bce0c24526a4af6
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2023-01-04 21:20:35 +0600

    Remove the <a> tag inside <Link> tag

--- components/LoginRequired.tsx
+++ components/LoginRequired.tsx
@@ -5,9 +5,7 @@ export default function LoginRequired() {
   return (
     <div id="login-reuired">
       <h1>Sorry, you need to be logged in to view this page.</h1>
-      <Link href="/login">
-        <a>Click here to login</a>
-      </Link>
+      <Link href="/login">Click here to login</Link>
     </div>
   );
 }
